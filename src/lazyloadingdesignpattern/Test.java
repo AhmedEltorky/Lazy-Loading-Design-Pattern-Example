425b151b9e415e3404505475557f99165587615b
AhmedEltorky <eng.ahmed.adel1991@gmail.com>
2018-08-16 10:25:05 +0200

    add some code.

--- src/lazyloadingdesignpattern/Test.java
+++ src/lazyloadingdesignpattern/Test.java
@@ -1,28 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lazyloadingdesignpattern;
 
 /**
  *
- * @author eng-torky
+ * @author Ahmed El-Torky
  */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         LazyLoadStudentData student01 = new LazyLoadStudentData();
         LazyLoadStudentData student02 = new LazyLoadStudentData();
         LazyLoadStudentData student03 = new LazyLoadStudentData();
         LazyLoadStudentData student04 = new LazyLoadStudentData();
-        
+
         student03.getStudentData().setStudentId("03"); //creation
         student03.getStudentData().setStudentName("Ali");
         student03.getStudentData().setAddress("Cairo,Egypt");
+        student03.getStudentData().setAge(12);
+        
+        System.out.println("- the student:" + student03.getStudentData().getStudentName() + " has " + student03.getStudentData().getAge() + " years old.");
     }
-    
+
 }
